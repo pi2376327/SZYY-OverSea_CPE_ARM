d5f4d1906d92f750e790419cb4f514962b75a645
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2022-03-18 21:39:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #3git clone https://github.com/pymumu/openwrt-smartdns.git /package/lean/smartdns
 #git clone https://github.com/pymumu/luci-app-smartdns.git /package/lean/luci-app-smartdns
 #git clone https://github.com/kuoruan/openwrt-frp.git /package/frp-pi
