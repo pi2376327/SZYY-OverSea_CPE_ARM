b76212ad14ad47b8500b4e78e5be342abdfef657
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2022-03-19 18:51:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,5 +12,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.150.1/g' package/base-files/files/bin/config_generate
-#sed -i 's/hostname='OpenWrt'/hostname='jywx'/g' package/base-files/files/bin/config_generate
+sed -i 's/hostname='OpenWrt'/hostname='jywx'/g' package/base-files/files/bin/config_generate
 #sed -i 's/root::0:0:99999:7:::/root:$1$kG.psLXF$eWnaXS9LQpId57E5ipfJO\/:18928:0:99999:7:::/g' package/base-files/files/etc/shadow
