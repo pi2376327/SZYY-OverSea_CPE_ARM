54ab1fe255d27074bb004599adfde55c2e4a880f
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2022-03-21 14:37:24 +0800

    Create newifi-diy-part1.sh

---
+++ newifi-diy-part1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#3git clone https://github.com/pymumu/openwrt-smartdns.git /package/lean/smartdns
+#git clone https://github.com/pymumu/luci-app-smartdns.git /package/lean/luci-app-smartdns
+#git clone https://github.com/kuoruan/openwrt-frp.git /package/frp-pi
+#git clone https://github.com/kuoruan/luci-app-frpc.git /package/luci-app-frpc-pi
+#git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
