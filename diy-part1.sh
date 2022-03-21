cad3946a06adf07f395f28e429bfaf8d658df351
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2022-03-21 23:22:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,5 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git /package/lean/
-git clone https://github.com/pymumu/luci-app-smartdns.git /package/lean/
+echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom' >>feeds.conf.default
+echo 'src-git luci-app-smartdns https://github.com/pymumu/luci-app-smartdns' >>feeds.conf.default
