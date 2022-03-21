c97a4aaf419ed01305c0e30a74c01585a3826cb5
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2022-03-21 21:45:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,4 +13,17 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.150.1/g' package/base-files/files/bin/config_generate
 sed -i 's/hostname='OpenWrt'/hostname='jywx'/g' package/base-files/files/bin/config_generate
+cat >package/base-files/files/etc/banner<<EOF
+  ____      _     ___   ____        ____  __
+ | __ )    | |   | \ \ / /\ \      / /\ \/ /
+ |  _ \ _  | |_  | |\ V /  \ \ /\ / /  \  / 
+ | |_) | |_| | |_| | | |    \ V  V /   /  \ 
+ |____/ \___/ \___/  |_|     \_/\_/   /_/\_\
+                                            
+---------------------------------------------------
+     Access For Last-Miles With Intetnet
+  
+           since 2015,www.edpn.com.cn
+---------------------------------------------------
+EOF
 #sed -i 's/root::0:0:99999:7:::/root:$1$kG.psLXF$eWnaXS9LQpId57E5ipfJO\/:18928:0:99999:7:::/g' package/base-files/files/etc/shadow
