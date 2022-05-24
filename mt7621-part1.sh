414dd91a65afe76db1faaec7818a6b723982ad29
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2022-05-24 17:44:32 +0800

    Update and rename newifi-diy-part1.sh to mt7621-part1.sh

--- mt7621-part1.sh
+++ mt7621-part1.sh
@@ -16,6 +16,3 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom' >>feeds.conf.default
-#echo 'src-git luciappsmartdns https://github.com/pymumu/luci-app-smartdns' >>feeds.conf.default
-echo 'src-git smartdns https://github.com/pi2376327/luci-app-smartdns' >>feeds.conf.default
