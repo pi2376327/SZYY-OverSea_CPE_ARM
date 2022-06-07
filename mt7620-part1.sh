9492153fbcf57b6561c9f943c5438e390a48ab1a
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2022-06-07 13:49:23 +0800

    Update mt7620-part1.sh

--- mt7620-part1.sh
+++ mt7620-part1.sh
@@ -20,4 +20,3 @@
 #----更改默认主题--------
 rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
