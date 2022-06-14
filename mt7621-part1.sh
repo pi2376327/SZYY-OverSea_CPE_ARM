8e4c226d8fc0be6e5777adf8f25c893e367fab82
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2022-06-14 15:28:03 +0800

    Update mt7621-part1.sh

--- mt7621-part1.sh
+++ mt7621-part1.sh
@@ -20,4 +20,4 @@
 #----更改默认主题--------
 rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config.git  /package/lean/luci-theme-argon-config
+git clone https://github.com/jerrykuku/luci-app-argon-config.git  package/lean/luci-theme-argon-config
