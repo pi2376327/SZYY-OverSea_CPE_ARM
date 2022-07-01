f62580adc38c0ad24e283a98729a716ad24d4adf
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2022-07-01 09:55:02 +0800

    Update x86-part2.sh

--- x86-part2.sh
+++ x86-part2.sh
@@ -12,7 +12,11 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.150.1/g' package/base-files/files/bin/config_generate
+
+#修改默认主机名
 sed -i 's/OpenWrt/JYWX-CPE/g' package/base-files/files/bin/config_generate
+
+#修改欢迎banner
 cat >package/base-files/files/etc/banner<<EOF
   ____      _     ___   ____        ____  __
  | __ )    | |   | \ \ / /\ \      / /\ \/ /
@@ -20,11 +24,20 @@ cat >package/base-files/files/etc/banner<<EOF
  | |_) | |_| | |_| | | |    \ V  V /   /  \ 
  |____/ \___/ \___/  |_|     \_/\_/   /_/\_\ 
                                             
----------------------------------------------------
+--------------------------------------------------------
      Access For Last-Miles With Intetnet
   
            since 2015,www.edpn.com.cn
----------------------------------------------------
+--------------------------------------------------------
 EOF
+
+#修改默认密码
 sed -i '/root::0/d' package/lean/default-settings/files/zzz-default-settings
 sed -i '1c\root:$1$KFkimD6C$KSpEWi1IcwqWYrESv2fQy/:19074:0:99999:7:::' package/base-files/files/etc/shadow
+
+#替换版本和名字
+sed -i 's/R22.3.13/R2022.6.14/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/OpenWrt/JYWX-CPE/g' package/lean/default-settings/files/zzz-default-settings
+
+#更改启用的默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
