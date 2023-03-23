148a6950b1e3a8466b5ea1a1136a0ad8e5b6ff95
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2023-03-23 12:25:38 +0800

    Update mt7621-part2.sh

--- mt7621-part2.sh
+++ mt7621-part2.sh
@@ -32,12 +32,19 @@ cat >package/base-files/files/etc/banner<<EOF
 EOF
 
 #修改默认密码
-sed -i '/root::0/d' package/lean/default-settings/files/zzz-default-settings
-sed -i '1c\root:$1$KFkimD6C$KSpEWi1IcwqWYrESv2fQy/:19074:0:99999:7:::' package/base-files/files/etc/shadow
+#sed -i '/root::0/d' package/lean/default-settings/files/zzz-default-settings
+#sed -i '1c\root:$1$KFkimD6C$KSpEWi1IcwqWYrESv2fQy/:19074:0:99999:7:::' package/base-files/files/etc/shadow
+sed -i 's#$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0#$1$KFkimD6C$KSpEWi1IcwqWYrESv2fQy/:19074#g' package/lean/default-settings/files/zzz-default-settings
 
-#替换版本和名字
-sed -i 's/R22.3.13/R2022.6.14/g' package/lean/default-settings/files/zzz-default-settings
+#替换版本和名字，以及设备型号
+sed -i 's/R23.3.3/R23.3.22/g' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/OpenWrt/JYWX-CPE/g' package/lean/default-settings/files/zzz-default-settings
+echo "sed -i 's/Zbtlink ZBT-WG3526/JYWX=WIFI-4G/g\' /proc/cpuinfo" >>  package/lean/default-settings/files/zzz-default-settings
 
 #更改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+
+#更改默认wifi ssid
+sed -i 's/set wireless.default_radio${devidx}.ssid=OpenWrt/set wireless.default_radio${devidx}.ssid=vpn/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/set wireless.default_radio${devidx}.encryption=none/set wireless.default_radio${devidx}.encryption=psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i '/psk2/a\                        set wireless.default_radio${devidx}.key=jywx.com' package/kernel/mac80211/files/lib/wifi/mac80211.sh
