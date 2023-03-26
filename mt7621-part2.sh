8541b65a91c0c2372077ac5a139df27ba48a4610
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2023-03-26 20:55:18 +0800

    Update mt7621-part2.sh

--- mt7621-part2.sh
+++ mt7621-part2.sh
@@ -33,13 +33,12 @@ EOF
 
 #修改默认密码
 #sed -i '/root::0/d' package/lean/default-settings/files/zzz-default-settings
-#sed -i '1c\root:$1$KFkimD6C$KSpEWi1IcwqWYrESv2fQy/:19074:0:99999:7:::' package/base-files/files/etc/shadow
-sed -i 's#$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0#$1$KFkimD6C$KSpEWi1IcwqWYrESv2fQy/:19074#g' package/lean/default-settings/files/zzz-default-settings
+sed -i '1c\root:$1$KFkimD6C$KSpEWi1IcwqWYrESv2fQy/:19074:0:99999:7:::' package/base-files/files/etc/shadow
 
 #替换版本和名字，以及设备型号
 sed -i 's/R23.3.3/R23.3.22/g' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/OpenWrt/JYWX-CPE/g' package/lean/default-settings/files/zzz-default-settings
-echo "sed -i 's/Zbtlink ZBT-WG3526/JYWX=WIFI-4G/g\' /proc/cpuinfo" >>  package/lean/default-settings/files/zzz-default-settings
+echo "sed -i 's/Zbtlink ZBT-WG3526/JYWX=WIFI-4G/g' /proc/cpuinfo" >>  package/lean/default-settings/files/zzz-default-settings
 
 #更改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
