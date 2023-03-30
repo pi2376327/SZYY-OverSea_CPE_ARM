a2cadf65ea5f98d476fefe51fa1053a8d7945b65
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2023-03-30 15:55:27 +0800

    Update mt7621-part1.sh

--- mt7621-part1.sh
+++ mt7621-part1.sh
@@ -18,6 +18,6 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 #----更改默认主题--------
-rm -rf package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+#rm -rf package/lean/luci-theme-argon
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 #git clone https://github.com/jerrykuku/luci-app-argon-config.git  package/lean/luci-app-argon-config
