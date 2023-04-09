8865972e9ace74e893240483b260952b6a1d28e3
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2023-04-09 20:59:49 +0800

    Update mt7621-part2.sh

--- mt7621-part2.sh
+++ mt7621-part2.sh
@@ -41,9 +41,7 @@ sed -i "/exit 0/i\sed -i \'s#Zbtlink ZBT-WG3526#JYWX-WIFI-4G#g\' \/proc\/cpuinfo
 
 #更改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
-rm -rf package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-#git clone https://github.com/jerrykuku/luci-app-argon-config.git  package/lean/luci-app-argon-config
+
 
 #更改默认wifi ssid
 sed -i 's/set wireless.default_radio${devidx}.ssid=OpenWrt/set wireless.default_radio${devidx}.ssid=vpn/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -53,4 +51,3 @@ sed -i '/psk2/a\                        set wireless.default_radio${devidx}.key=
 #更改wan口默认dns
 sed -i "/exit 0/i\sed -i \'\/option proto '\\\''dhcp'\\\''\/a\\\        option dns '\\\''172\.18\.0\.1'\\\''\' \/etc\/config\/network"   package/lean/default-settings/files/zzz-default-settings
 sed -i "/exit 0/i\sed -i \'\/option proto '\\\''dhcp'\\\''\/a\\\        option peerdns '\\\''0'\\\''\' \/etc\/config\/network"   package/lean/default-settings/files/zzz-default-settings
-
