6534c3ed7861af822d485b4fc9a62e197b7637b4
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2023-04-09 16:56:39 +0800

    Update mt7621-part2.sh

--- mt7621-part2.sh
+++ mt7621-part2.sh
@@ -41,6 +41,9 @@ sed -i "/exit 0/i\sed -i \'s#Zbtlink ZBT-WG3526#JYWX-WIFI-4G#g\' \/proc\/cpuinfo
 
 #更改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+#git clone https://github.com/jerrykuku/luci-app-argon-config.git  package/lean/luci-app-argon-config
 
 #更改默认wifi ssid
 sed -i 's/set wireless.default_radio${devidx}.ssid=OpenWrt/set wireless.default_radio${devidx}.ssid=vpn/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
