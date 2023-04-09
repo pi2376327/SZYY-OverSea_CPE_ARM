c03a0293bcc033d7c59db06ad494f5b86e290a5e
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2023-04-09 16:57:35 +0800

    Update openvpn-up.sh

--- files/etc/openvpn/openvpn-up.sh
+++ files/etc/openvpn/openvpn-up.sh
@@ -2,5 +2,5 @@
 src=$(ip addr show tun0 | grep 'inet ' | sed 's/^.*inet //g' | sed 's/\/16.*$//g')
 iptables -t mangle -I PREROUTING -m set ! --match-set chnroute dst -j MARK --set-mark 1
 iptables -t mangle -I OUTPUT -m set ! --match-set chnroute dst -j MARK --set-mark 1
-ip route add default via 172.18.0.1 dev tun0 src $src table 100 
+ip route add default via 172.16.0.1 dev tun0 src $src table 100 
 ip rule add from all fwmark 1 table 100
