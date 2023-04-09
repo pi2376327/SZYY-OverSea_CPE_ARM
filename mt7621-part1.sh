ffe07400f0c02380500803703ab88bc7b05d1f54
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2023-04-10 00:27:18 +0800

    Update mt7621-part1.sh

--- mt7621-part1.sh
+++ mt7621-part1.sh
@@ -21,3 +21,7 @@
 rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 #git clone https://github.com/jerrykuku/luci-app-argon-config.git  package/lean/luci-app-argon-config
+
+#编译frp软件
+#rm -rf package/lean/frpc
+
