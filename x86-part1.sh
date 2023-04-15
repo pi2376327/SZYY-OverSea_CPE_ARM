65a6b14044bbfa2c0b8dc98f5b634877c9d6ff3d
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2023-04-15 21:22:40 +0800

    Update x86-part1.sh

--- x86-part1.sh
+++ x86-part1.sh
@@ -21,7 +21,7 @@
 rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 #git clone https://github.com/jerrykuku/luci-app-argon-config.git  package/lean/luci-app-argon-config
-git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom /package/lean/luci-theme-infinityfreedom
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git /package/lean/luci-theme-infinityfreedom
 
 #编译frp软件
 rm -rf package/lean/frp
