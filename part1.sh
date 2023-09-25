24cc90c3d8b0738720ff556c329dc65d914f1505
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2023-09-25 21:40:48 +0800

    Update and rename ZBT-16M-part1.sh to part1.sh

--- part1.sh
+++ part1.sh
@@ -18,8 +18,8 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 #删掉默认主题&&下载新主题
-rm -rf package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+#rm -rf package/lean/luci-theme-argon
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 #git clone https://github.com/jerrykuku/luci-app-argon-config.git  package/lean/luci-app-argon-config
 
 #编译frp软件
