ea4554f9381565d23dc129b1ed1ea6c187fad2bf
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2023-09-26 09:55:44 +0800

    Update openvpn-up.sh

--- files/etc/openvpn/openvpn-up.sh
+++ files/etc/openvpn/openvpn-up.sh
@@ -1,6 +1,30 @@
 #!/bin/sh
+
+iptables -t mangle -C PREROUTING -m set ! --match-set chnroute dst -j MARK --set-mark 1
+if [ $? = 0 ]; then
+        echo "------------the PREROUTING rules alread exist"
+        iptables -t mangle -C OUTPUT -m set ! --match-set chnroute dst -j MARK --set-mark 1
+        if [ $? = 0 ]; then
+                echo "------------the OUTPUT rules alread exist"
+                exit 0
+        else
+                iptables -t mangle -I OUTPUT -m set ! --match-set chnroute dst -j MARK --set-mark 1
+                echo "------------add OUTPUT  rules------------------"
+        fi
+else
+        iptables -t mangle -I PREROUTING -m set ! --match-set chnroute dst -j MARK --set-mark 1
+        echo "------------add PREROUTING rules------------------"
+
+        iptables -t mangle -C OUTPUT -m set ! --match-set chnroute dst -j MARK --set-mark 1
+        if [ $? = 0 ]; then
+                echo "------------the OUTPUT rules alread exist"
+                exit 0
+        else
+                iptables -t mangle -I OUTPUT -m set ! --match-set chnroute dst -j MARK --set-mark 1
+                echo "------------add OUTPUT  rules------------------"
+        fi
+fi
+
 src=$(ip addr show tun0 | grep 'inet ' | sed 's/^.*inet //g' | sed 's/\/16.*$//g')
-iptables -t mangle -I PREROUTING -m set ! --match-set chnroute dst -j MARK --set-mark 1
-iptables -t mangle -I OUTPUT -m set ! --match-set chnroute dst -j MARK --set-mark 1
 ip route add default via 172.16.0.1 dev tun0 src $src table 100 
 ip rule add from all fwmark 1 table 100
