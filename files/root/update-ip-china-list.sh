949d81924bf6168599996c8c08752127f7b34304
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2024-01-06 14:10:14 +0800

    Update update-ip-china-list.sh

--- files/root/update-ip-china-list.sh
+++ files/root/update-ip-china-list.sh
@@ -1,28 +1,40 @@
 #!/bin/sh
+  
+DATE=`date +%Y-%m-%d-%H:%M:%S`
 
-#创建临时目录并下载ip地址源文件
+#Create tmp directory and download source file of domain
 mkdir -p /tmp/china-ip/
 wget -P /tmp/china-ip/ https://raw.githubusercontent.com/metowolf/iplist/master/data/special/china.txt
 
-#删除空行
-grep -vE '^#|^&}' /tmp/china-ip/china.txt  > /tmp/china-ip/ip.txt
+if [ ! -f /tmp/china-ip/china.txt ];then
+        echo $DATE: IPS file download failed, EXIT !
+        exit 0
+else
+        echo $DATE: IPs file download successful.
+        #Delete empty row
+        grep -vE '^#|^&}' /tmp/china-ip/china.txt  > /tmp/china-ip/ip.txt
 
-#转换为ipset格式，并移动至目标目录
-sed -i 's/^/add chnroute /g' /tmp/china-ip/ip.txt
-mv -f /tmp/china-ip/ip.txt /etc/openvpn/chnroute-ipset
+        #Convert to ipse format and move to directory of openvpn
+        sed -i 's/^/add chnroute /g' /tmp/china-ip/ip.txt
+        mv -f /tmp/china-ip/ip.txt /etc/openvpn/chnroute-ipset
+        echo $DATE: Format conversion done
 
-#添加私有ip网段
-echo "add chnroute 10.0.0.0/8" >> /etc/openvpn/chnroute-ipset
-echo "add chnroute 172.16.0.0/12" >> /etc/openvpn/chnroute-ipset
-echo "add chnroute 192.168.0.0/16" >> /etc/openvpn/chnroute-ipset
+        #Add private IPs to ipset list
+        echo "add chnroute 10.0.0.0/8" >> /etc/openvpn/chnroute-ipset
+        echo "add chnroute 172.16.0.0/12" >> /etc/openvpn/chnroute-ipset
+        echo "add chnroute 192.168.0.0/16" >> /etc/openvpn/chnroute-ipset
+        echo $DATE: Add private networks to ipset seccessfully
 
-#ipset清空原数据并加载新配置
-ipset flush chnroute
-ipset restore -f /etc/openvpn/chnroute-ipset
+        #Reload new ipset list
+        ipset flush chnroute
+        ipset restore -f /etc/openvpn/chnroute-ipset
+        echo $DATE: loading ipset list successfully
 
-#删除临时文件
-rm -rf /tmp/china-ip
+        #Delete tmp
+        rm -rf /tmp/china-ip
 
-echo '--------------------------------------'
-echo 'update ipv4 date from apnic completely'
-echo -e '--------------------------------------\n\n'
+        echo '-----------------------------------------------------------------'
+        echo $DATE: Update ipset list from github completely in `date +%Y-%m-%d`
+        echo '-----------------------------------------------------------------'
+        exit 0
+fi
