d4533fbad980cc399e892da7c260b7b00cbdd489
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2024-01-06 18:55:27 +0800

    Update openvpn-up.sh

--- files/etc/openvpn/openvpn-up.sh
+++ files/etc/openvpn/openvpn-up.sh
@@ -5,30 +5,33 @@ src=$(ip addr show tun0 | grep 'inet ' | sed 's/^.*inet //g' | sed 's/\/16.*$//g
 gw=$(ip addr show tun0 | grep 'inet ' | cut -d' ' -f6 | cut -d'.' -f1-3).1
 ip route add default via $gw dev tun0 src $src table 100 
 ip rule add from all fwmark 1 table 100
+#ip route add 8.8.8.8 via $gw dev tun0 src $src
 
-#check and add rules of mangle
+#check and add rules of mangle     
+DATE=`date +%Y-%m-%d-%H:%M:%S`
 iptables -t mangle -C PREROUTING -m set ! --match-set chnroute dst -j MARK --set-mark 1
 if [ $? = 0 ]; then
-        echo "------------the PREROUTING rules alread exist"
+        echo "$DATE: The PREROUTING rules alread exist" 
         iptables -t mangle -C OUTPUT -m set ! --match-set chnroute dst -j MARK --set-mark 1
         if [ $? = 0 ]; then
-                echo "------------the OUTPUT rules alread exist"
+                echo "$DATE: The OUTPUT rules alread exist"
                 exit 0
         else
                 iptables -t mangle -I OUTPUT -m set ! --match-set chnroute dst -j MARK --set-mark 1
-                echo "------------add OUTPUT  rules------------------"
+                echo "$DATE: Add OUTPUT  rules"
+                exit 0
         fi
 else
         iptables -t mangle -I PREROUTING -m set ! --match-set chnroute dst -j MARK --set-mark 1
-        echo "------------add PREROUTING rules------------------"
+        echo "$DATE: Add PREROUTING rules"
 
         iptables -t mangle -C OUTPUT -m set ! --match-set chnroute dst -j MARK --set-mark 1
         if [ $? = 0 ]; then
-                echo "------------the OUTPUT rules alread exist"
+                echo "$DATE: The OUTPUT rules alread exist"
                 exit 0
         else
                 iptables -t mangle -I OUTPUT -m set ! --match-set chnroute dst -j MARK --set-mark 1
-                echo "------------add OUTPUT  rules------------------"
+                echo "$DATE: Add OUTPUT  rules"
+                exit 0
         fi
 fi
-
