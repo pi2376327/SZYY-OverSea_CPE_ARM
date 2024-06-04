de027b04b500b4d276c67ca73be6312d596e6cf9
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2024-06-04 23:40:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,23 +16,3 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-#清理之前的编译文件
-#make clean
-
-#删掉默认主题&&下载新主题
-#lean-lede
-#rm -rf package/lean/luci-theme-argon
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-#git clone https://github.com/jerrykuku/luci-app-argon-config.git  package/lean/luci-app-argon-config
-
-#openwrt #CONFIG_PACKAGE_luci-theme-argon=y
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-
-#替换编译luci-app-xfrpc软件
-rm -rf package/luci-app-xfrpc
-git clone https://github.com/liudf0716/luci-app-xfrpc.git  package/luci-app-xfrpc
-#git clone https://github.com/kuoruan/openwrt-frp.git package/lean/frp
-
-#添加autoreboot插件
-#git clone https://github.com/f8q8/luci-app-autoreboot.git package/luci-app-autoreboot
