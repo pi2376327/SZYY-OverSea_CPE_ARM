e32c2a97596cbc454af72506364d43c0b0396533
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2024-06-06 20:22:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,24 +9,3 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
-#删掉默认主题&&下载新主题
-#lean-lede
-#rm -rf package/lean/luci-theme-argon
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-#git clone https://github.com/jerrykuku/luci-app-argon-config.git  package/lean/luci-app-argon-config
-
-#下载frp
-#git clone https://github.com/kuoruan/openwrt-frp.git package/lean/frp
-
-#openwrt #CONFIG_PACKAGE_luci-theme-argon=y
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-
-#替换编译luci-app-xfrpc软件
-#rm -rf /package/feeds/packages/xfrpc
-#git clone https://github.com/liudf0716/xfrpc.git package/xfrpc
-rm -rf package/feeds/luci/luci-app-xfrpc
-git clone https://github.com/liudf0716/luci-app-xfrpc.git  package/feeds/luci/luci-app-xfrpc
-
-#添加autoreboot插件
-#git clone https://github.com/f8q8/luci-app-autoreboot.git package/luci-app-autoreboot
