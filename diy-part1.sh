cf3abe2d49211dd33e828f978c1964fb47a05fdc
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2024-06-06 20:23:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,24 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+#删掉默认主题&&下载新主题
+#lean-lede
+#rm -rf package/lean/luci-theme-argon
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+#git clone https://github.com/jerrykuku/luci-app-argon-config.git  package/lean/luci-app-argon-config
+
+#openwrt #CONFIG_PACKAGE_luci-theme-argon=y
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+
+#替换编译luci-app-xfrpc软件
+#rm -rf /package/feeds/packages/xfrpc
+#git clone https://github.com/liudf0716/xfrpc.git package/xfrpc
+rm -rf package/feeds/luci/luci-app-xfrpc
+git clone https://github.com/liudf0716/luci-app-xfrpc.git  package/feeds/luci/luci-app-xfrpc
+
+#下载原版frp
+#git clone https://github.com/kuoruan/openwrt-frp.git package/lean/frp
+
+#添加autoreboot插件
+#git clone https://github.com/f8q8/luci-app-autoreboot.git package/luci-app-autoreboot
