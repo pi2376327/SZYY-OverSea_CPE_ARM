c0e4daff2a719346493dabb9af5d4f723e11ea3c
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2024-06-09 21:49:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,8 +22,8 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-a
 #替换编译luci-app-xfrpc软件
 #rm -rf /package/feeds/packages/xfrpc
 #git clone https://github.com/liudf0716/xfrpc.git package/xfrpc
-rm -rf package/feeds/luci/luci-app-xfrpc
-git clone https://github.com/liudf0716/luci-app-xfrpc.git  package/feeds/luci/luci-app-xfrpc
+#rm -rf package/feeds/luci/luci-app-xfrpc
+#git clone https://github.com/liudf0716/luci-app-xfrpc.git  package/feeds/luci/luci-app-xfrpc
 
 #下载原版frp
 #git clone https://github.com/kuoruan/openwrt-frp.git package/lean/frp
