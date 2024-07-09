491ca40317320ccf9172c8ae954eaccb4af7ca6c
pi2376327 <82651004+pi2376327@users.noreply.github.com>
2024-07-10 00:17:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -31,3 +31,6 @@ git clone https://github.com/liudf0716/luci-app-xfrpc.git  feeds/luci/applicatio
 #添加autoreboot插件
 #git clone https://github.com/f8q8/luci-app-autoreboot.git package/luci-app-autoreboot
 
+#添加redsocks2插件
+git clone https://github.com/will-cxydiary/redsocks2-openwrt.git package/redsocks2
+
